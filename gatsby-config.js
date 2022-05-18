cb25276bb2f2932da84820a366e56ade0d17cef9
Jorge Escobedo <jescob@gmail.com>
2022-05-17 23:32:14 -0400

    Upload initial version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,38 @@
 module.exports = {
   siteMetadata: {
-    title: `Jorge Escobedo's Blog`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteTitle: `Jorge Escobedo`,
+    siteTitleAlt: `Jorge Escobedo`,
+    siteHeadline: `Jorge Escobedo's Blog`,
+    siteUrl: `https://www.jorgeescobedo.com`,
+    siteDescription: `Jorge Escobedo's Blog`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `@coco_escobedo`
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      options: {
+        navigation: [
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/coco_escobedo`,
+          },
+          {
+            name: `LinkedIn`,
+            url: `https://linkedin.com/in/jescob`,
+          }
+        ],
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-theme-ui`
+  ],
 }
